90281b95c7f64c1bb28561844bb9d739c78b045a
unknown <malou95@gmx.de>
2024-03-10 15:24:49 +0100

    remove default views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import ItemGenerator from '../components/item/Itemgenerator.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ItemGenerator />
   </main>
 </template>
